256f7430ac691d04e53688071ad4329ccd272276
Thomas Herbeth <thomas.herbeth@gmail.com>
2023-04-14 22:15:34 +0200

    [+] DataStore file initialization.

--- app/src/main/java/com/booboot/vndbandroid/app/di/Koin.kt
+++ app/src/main/java/com/booboot/vndbandroid/app/di/Koin.kt
@@ -1,13 +1,21 @@
 package com.booboot.vndbandroid.app.di
 
 import android.app.Application
+import com.booboot.vndbandroid.feature.vnlist.di.vnListModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import java.io.File
 
 val appModule = module {
+    includes(vnListModule)
+
+    single(named("datastore_preferences_file")) {
+        File(androidContext().filesDir, "datastore_v4.preferences_pb")
+    }
 }
 
 fun Application.setupKoin(vararg modules: Module) = startKoin {
